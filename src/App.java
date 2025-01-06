58336cd1c01ea033f6aec297e5719f8cb8848bfc
Kanda0n <kononchenko1996@gmail.com>
2025-01-06 22:20:19 +0300

    Project completed

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Классы");
+        Author one = new Author("Евгений", "Замятин");
+        Author two = new Author("Джордж", "Оруэлл");
+        Book first = new Book("1984", two, 1934);
+        Book second = new Book("МЫ", one, 1924);
+        System.out.println("Первая книга на полке: '" + first.getName() + "', " +
+                two.getSurname() + ". Год издания, " + first.getYear());
+        // System.out.println("Вторая книга на полке: '" + second.getName() + "', " +
+        // Хочу через Воок получать фамилию автора,
+        // не понимаю, как можно сделать || second.getAuthor() + ". Год издания, " + second.getYear());
+        System.out.println("Вторая книга на полке: '" + second.getName() + "', " +
+                one.getSurname() + ". Год издания, " + second.getYear());
+        second.setYear(1991);
+        System.out.println("Третья книга на полке: '" + second.getName() + "', " +
+                one.getSurname() + ". Год издания, " + second.getYear());
+    }
+}
