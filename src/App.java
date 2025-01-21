4006ba0f812b4ca04a34784bf423f708feda4bb1
Kanda0n <kononchenko1996@gmail.com>
2025-01-21 13:09:53 +0300

    Homework 13 completed

--- src/App.java
+++ src/App.java
@@ -5,12 +5,15 @@ public class App {
         Author two = new Author("Джордж", "Оруэлл");
         Book first = new Book("1984", two, 1934);
         Book second = new Book("МЫ", one, 1924);
-        System.out.println("Первая книга на полке: '" + first.getName() + "', " +
-                first.getAuthor().getSurname() + ". Год издания, " + first.getYear());
-        System.out.println("Вторая книга на полке: '" + second.getName() + "', " +
-                second.getAuthor().getSurname() + ". Год издания, " + second.getYear());
+        // System.out.println("Первая книга на полке: '" + first.getName() + "', " +
+        //         first.getAuthor().getSurname() + ". Год издания, " + first.getYear());
+        System.out.println(first.toString());
+        // System.out.println("Вторая книга на полке: '" + second.getName() + "', " +
+        //       second.getAuthor().getSurname() + ". Год издания, " + second.getYear());
+        System.out.println(second.toString());
         second.setYear(1991);
-        System.out.println("Третья книга на полке: '" + second.getName() + "', " +
-                second.getAuthor().getSurname() + ". Год издания, " + second.getYear());
+        // System.out.println("Третья книга на полке: '" + second.getName() + "', " +
+        //       second.getAuthor().getSurname() + ". Год издания, " + second.getYear());
+        System.out.println(second.toString());
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
@@ -23,5 +25,26 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Книга на полке: '" +
+                name + '\'' +
+                ", Автор " + author +
+                ", год издания " + year +
+                ".";
     }
 }
